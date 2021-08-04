e2c9f8647cd7f225acc57d8a2022982ebf034001
Fabian-Ome <fabiantabla@hotmail.com>
2021-08-03 20:25:48 -0500

    base CRUD apiRest finish

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -4,6 +4,7 @@ const router = Router();
 
 // dataBase connection
 import { connect } from '../database'
+import { ObjectId } from 'mongodb';
 
 router.get('/', async (req, res) => {
 
@@ -12,5 +13,46 @@ router.get('/', async (req, res) => {
     console.log(result);
     res.json(result);
 });
+
+router.post('/', async (req, res) => {
+    const db = await connect();
+    // const {title, description } = req.body;
+    const Task = {
+        title: req.body.title,
+        description: req.body.description
+    }
+    const result = await db.collection('tasks').insertOne(Task);
+    res.json(result);
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const db = await connect();
+    const result = await db.collection('tasks').findOne({ _id: ObjectId(id) });
+    res.json(result);
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const db = await connect();
+    const result = await db.collection('tasks').deleteOne({ _id: ObjectId(id) });
+    res.json({
+        Message: `Task ${id} deleted`,
+        result
+    })
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const updateTask = {
+        title: req.body.title,
+        description: req.body.description
+    };
+    const db = await connect();
+    await db.collection('tasks').updateOne({ _id: ObjectId(id) }, { $set: updateTask });
+    res.json({
+        Message: `Task ${id} updated`
+    });
+})
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
+// import { json } from 'express';
 
 const app = express();
 
 //routes
 import IndexRoutes from './routes/index.routes'
 import TaskRoutes from './routes/tasks.routes';
+import { json } from 'express';
 //settings 
 app.set('port', process.env.PORT || 3000);
 
-
+//middlewares
+// app.use(json());
+app.use(express.json());
 
 //routes
 app.use(IndexRoutes);
